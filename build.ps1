59611941d04ee20d50d0b9942c142bc0423f18b4
Chris Rogers <chris@portchris.co.uk>
2018-11-13 20:49:43 +0000

    Add Powershell scripts for dirty arse Windows users (Michael)

---
+++ build.ps1
@@ -0,0 +1,7 @@
+﻿# This script build the docker images for the the environment.
+# Usage: ./build.sh <Additional argurments>
+
+# Get Path parameter
+Param([String]$args)
+
+docker-compose -f docker-compose.yml build --build-arg UID=1000 --build-arg GID=1000 $args
---
+++ start.ps1
@@ -0,0 +1,8 @@
+﻿
+# This script starts up the environment, be sure to run the ./build.ps1 script first if no images exist.
+# Usage: ./start.sh <Additional argurments>
+
+# Get Path parameter
+Param([String]$args)
+
+docker-compose up -d --remove-orphans $args
